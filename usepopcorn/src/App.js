be0c90bb5fb62c610e560cb792c014be83cd08ea
M-Abeer <102969595+M-Abeer@users.noreply.github.com>
2024-04-01 22:54:30 +0500

    pop

--- usepopcorn/src/App.js
+++ usepopcorn/src/App.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import StarRating from "./StarRating";
 
 const tempMovieData = [
   {
@@ -70,7 +71,8 @@ export default function App() {
   // });
 
   function handleSelectMovie(id) {
-    setSelectedId(id);
+    // setSelectedId(id);
+    setSelectedId((selectedId) => (id === selectedId ? null : id));
   }
   function handleCloseMovie() {
     setSelectedId(null);
@@ -78,7 +80,7 @@ export default function App() {
   useEffect(() => {
     async function fetchData() {
       try {
-        console.log(query);
+        // console.log(query);
         setError("");
         setIsLoader(true);
         const res = await fetch(
@@ -88,7 +90,7 @@ export default function App() {
           throw new error("Something went wrong with fetching movies");
         const data = await res.json();
         if (data.Response === "False") throw new Error("Movie not found");
-        console.log(data.Search);
+        // console.log(data.Search);
         setMovies(data.Search);
       } catch (err) {
         setError(err.message);
@@ -120,14 +122,18 @@ export default function App() {
           )}
           {error && <ErrorMessage message={error} />}
         </ListBox>
-        <ListBox movies={movies}>
-          {selectedId ? (
-            <MovieDetails
-              selectedId={selectedId}
-              handleCloseMovie={handleCloseMovie}
-            />
-          ) : null}
-        </ListBox>
+        {selectedId ? (
+          <ListBox movies={movies}>
+            {selectedId ? (
+              <MovieDetails
+                selectedId={selectedId}
+                handleCloseMovie={handleCloseMovie}
+              />
+            ) : null}
+          </ListBox>
+        ) : (
+          <Watched />
+        )}
       </Main>
     </>
   );
@@ -202,22 +208,69 @@ const ListBox = ({ children }) => {
 };
 function MovieDetails({ selectedId, handleCloseMovie }) {
   const [movie, setMovie] = useState({});
+  const [isLoader, setIsLoader] = useState(false);
+  const {
+    Title: title,
+    Year: year,
+    Poster: poster,
+    Runtime: runtime,
+    imdbRating,
+    Plot: plot,
+    Released: released,
+    Actors: actors,
+    Director: director,
+    Genre: genre,
+  } = movie;
+  // console.log(title, year);
+  //Pehle movie empty ho gi to display undefined
+  // then due to useEffect value occurs
   useEffect(() => {
+    setIsLoader(true);
     async function getMovieDetails() {
       const res = await fetch(
         `http://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`
       );
       const data = await res.json();
       setMovie(data);
+      setIsLoader(false);
     }
     getMovieDetails();
-  }, []);
+  }, [movie]);
   return (
     <div className="details">
-      <button className="btn-back" onClick={handleCloseMovie}>
-        &larr;
-      </button>
-      {selectedId}
+      <>
+        <header>
+          <button onClick={handleCloseMovie} className="btn-back">
+            &larr;
+          </button>
+          <img src={poster} alt={`Poster of ${movie} movie`} />
+          <div className="details-overview">
+            <h2>{title}</h2>
+            <p>
+              {released} &bull; {runtime}
+            </p>
+            <p>{genre}</p>
+            <p>
+              <span>⭐️</span>
+              {imdbRating} IMDb rating
+            </p>
+          </div>
+        </header>
+
+        <section>
+          <div
+            className="rating
+        "
+          >
+            <StarRating maxRating={10} size={24} />
+          </div>
+          <p>
+            <em>{plot}</em>
+          </p>
+          <p>Starring {actors}</p>
+          <p>Directed by {director}</p>
+        </section>
+      </>
     </div>
   );
 }
